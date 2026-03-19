f22bbe330e30fe1675aa5883bd9b2c69a28060fa
DefinitelyProfessional <lockedin.cs.555@gmail.com>
2026-03-19 23:06:17 +0700

    minor changes

---
+++ codes/hr-rational-sum/ratio-sum.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdint>
+
+using namespace std;
+
+int main(void) {
+    uint16_t a_, b_, n_; cin >> n_;
+    for (uint16_t i_ = 0; i_ < n_; i_++) {
+        cin >> a_ >> b_;
+        
+    }
+    return 0;
+}
