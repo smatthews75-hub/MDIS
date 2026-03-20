b4bfff54a2a7b377fac8a402e578214cf7b698cf
DefinitelyProfessional <lockedin.cs.555@gmail.com>
2026-03-20 11:18:11 +0700

    bruh 10 out of 100 test cases for ratio still failed

--- codes/hr-rational-sum/ratio-sum.cpp
+++ codes/hr-rational-sum/ratio-sum.cpp
@@ -1,27 +1,50 @@
 #include <iostream>
 #include <cstdint>
+#include <string>
 #include <algorithm>
 
 using namespace std;
 
+inline string big_print(__uint128_t x_) {
+    if (x_ == 0) {return "0";}
+    string s_; s_.reserve(100);
+    while (x_ > 0) {
+        s_.push_back('0' + (int)(x_ % 10));
+        x_ /= 10;
+    }
+    reverse(s_.begin(), s_.end());
+    return s_;
+}
+
+// custom gcd to handle super-massive numbers muse reference
+inline __uint128_t gcd128(__uint128_t a_, __uint128_t b_) {
+    __uint128_t tmp;
+    while (b_ != 0) {
+        tmp = a_ % b_;
+        a_ = b_;
+        b_ = tmp;
+    } return a_;
+}
+
 int main(void) {
-    uint64_t  a_, b_;
-    __uint128_t prev_a, prev_b, GCD, LCM;
+    uint64_t  a_buffer, b_buffer;
+    __uint128_t prev_a, prev_b, GCD, LCM, a_, b_;
     uint16_t  n_, n_loop; cin >> n_;
 
-    cin >> a_ >> b_; // get the first ratio
-    prev_a = a_; prev_b = b_;
+    cin >> a_buffer >> b_buffer; // get the first ratio
+    prev_a = a_buffer; prev_b = b_buffer;
     n_loop = n_ - 1; // loop through all ratio
     for (uint16_t i_ = 0; i_ < n_loop; i_++) {
-        cin >> a_ >> b_; // get ratio
+        cin >> a_buffer >> b_buffer; // get ratio
+        a_ = a_buffer; b_ = b_buffer;
         // use LCM to minimize denominator growth maybe
-        LCM = (prev_b / __gcd((uint64_t)prev_b, b_)) * b_; // think !
+        LCM = (prev_b / gcd128(prev_b, b_)) * b_; // think !
         prev_a = prev_a*(LCM / prev_b) + a_*(LCM / b_);
         prev_b = LCM;
         // reduce
-        GCD = __gcd(prev_a, prev_b);
+        GCD = gcd128(prev_a, prev_b);
         prev_a /= GCD; prev_b /= GCD;
     }
-    cout << (uint64_t)prev_a << " " << (uint64_t)prev_b << endl;
+    cout << big_print(prev_a) << " " << big_print(prev_b) << endl;
     return 0;
 }
