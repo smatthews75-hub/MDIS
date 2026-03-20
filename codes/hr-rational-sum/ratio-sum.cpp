bd74c74ceed0d66db184d424af9e7873a70e76a6
DefinitelyProfessional <lockedin.cs.555@gmail.com>
2026-03-20 10:23:41 +0700

    ratio-sum is mid

--- codes/hr-rational-sum/ratio-sum.cpp
+++ codes/hr-rational-sum/ratio-sum.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <cstdint>
+#include <algorithm>
 
 using namespace std;
 
 int main(void) {
-    uint16_t a_, b_, n_; cin >> n_;
-    for (uint16_t i_ = 0; i_ < n_; i_++) {
-        cin >> a_ >> b_;
-        
+    uint64_t  a_, b_;
+    __uint128_t prev_a, prev_b, GCD, LCM;
+    uint16_t  n_, n_loop; cin >> n_;
+
+    cin >> a_ >> b_; // get the first ratio
+    prev_a = a_; prev_b = b_;
+    n_loop = n_ - 1; // loop through all ratio
+    for (uint16_t i_ = 0; i_ < n_loop; i_++) {
+        cin >> a_ >> b_; // get ratio
+        // use LCM to minimize denominator growth maybe
+        LCM = (prev_b / __gcd((uint64_t)prev_b, b_)) * b_; // think !
+        prev_a = prev_a*(LCM / prev_b) + a_*(LCM / b_);
+        prev_b = LCM;
+        // reduce
+        GCD = __gcd(prev_a, prev_b);
+        prev_a /= GCD; prev_b /= GCD;
     }
+    cout << (uint64_t)prev_a << " " << (uint64_t)prev_b << endl;
     return 0;
 }
